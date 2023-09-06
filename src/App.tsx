c46f754ebe814044c4bf54331e90ca75a8a9a457
edilsonfj <edilson.desenvolved@gmail.com>
2023-09-06 14:57:12 -0300

    feat: Configuração de rotas no arquivo app.tsx, tailwind.config e main.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <h1>Edilson de Freitas Júnior</h1>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/skills" element={<h1>skills</h1>} />
+        <Route path="/projects" element={<h1>projects</h1>} />
+        <Route path="/about" element={<h1>about</h1>} />
+        <Route path="/contact" element={<h1>contact</h1>} />
+        <Route path="*" element={<h1>404 - Page not found</h1>} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
